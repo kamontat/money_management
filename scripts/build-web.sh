4f88e35ce36adfcff9a30d30f9f153b085183e66
Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>
2025-02-01 17:15:29 +0700

    perf(web): add script to build web wasm
    Signed-off-by: Kamontat Chantrachirathumrong <14089557+kamontat@users.noreply.github.com>

---
+++ scripts/build-web.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -e
+
+# shellcheck disable=SC1091
+source "$(dirname "$0")/_utils.sh"
+
+_prepare
+
+_flutter build web \
+  --wasm \
+  --source-maps \
+  --optimization-level 4 \
+  --pwa-strategy offline-first
+
+_exec cp -r "$_BUILD_WEB_PATH" \
+  "$_TARGET_WEB_PATH"
